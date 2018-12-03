92107a10c093873d09e40285ea905ee12a4d18db
datngohavan <ngohavandat93ndc@gmail.com>
2018-12-03 16:00:07 +0700

    read data to table market

--- Exchange-Demark/front-end/app/components/market/Market.jsx
+++ Exchange-Demark/front-end/app/components/market/Market.jsx
@@ -3,7 +3,7 @@ import { Table } from 'react-bootstrap';
 import firebase from 'firebase';
 
 let Market = React.createClass({
-    
+
     componentWillMount() {
         // Load custom in main and overrides
         require("../../css/main.less");
@@ -14,19 +14,20 @@ let Market = React.createClass({
         //     projectId: "demark-dtbs",
         //     messagingSenderId: "518328352226"             
         // });
-        
+
     },
 
-    readFromDtbsToTable(){
+    readFromDtbsToTable() {
         console.log("nghien oc cho");
-        var index =1;
+        var index = 1;
+        var tblTokensList = document.getElementById('tbl_tokens_list');
         var databaseRef = firebase.database().ref("/tokens");
-        databaseRef.once('value', function(snapshot){
-            snapshot.forEach(function (childSnapshot){
+        databaseRef.once('value', function (snapshot) {
+            snapshot.forEach(function (childSnapshot) {
                 var childKey = childSnapshot.key;
                 var childData = childSnapshot.val();
 
-                var row = document.getElementById('tbl_tokens_list');
+                var row = tblTokensList.insertRow(index);
                 var cellId = row.insertCell(0);
                 var cellName = row.insertCell(1);
                 var cellSymbol = row.insertCell(2);
@@ -38,7 +39,7 @@ let Market = React.createClass({
                 cellSymbol.appendChild(document.createTextNode(childData.symbol));
                 cellAddress.appendChild(document.createTextNode(childData.address));
                 cellDescription.appendChild(document.createTextNode(childData.description));
-                
+
                 index++;
 
             });
@@ -46,49 +47,51 @@ let Market = React.createClass({
     },
 
 
-    render () {
+    render() {
         return (
             <div className="market-table panel panel-default">
                 <div className="container-fluid">
                     <div className="row panel-heading">
                         <h1 className="panel-title">Token and ICO database</h1>
                         {/* {!this.props.market.error && ( */}
-                            <hr/>
-                            <Table id="tbl_tokens_list" striped condensed hover>
-                                <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Name</th>
-                                        <th>Symbol</th>
-                                        <th>Address</th>
-                                        <th>Description</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                        <tr>
-                                            <td className="style-row">1</td>
-                                            <td className="style-row color-token-name">DTU Token</td>
-                                            <td className="style-row">DTUK</td>
-                                            <td className="style-row">0xE41d2489571d322189246DaFA5ebDe1F4699F498</td>
-                                            <td className="style-row">Tokens for tuition fees at Duy Tan universtiy</td>
-                                        </tr>
-                                 
-                                    <tr>
-                                        <td className="style-row">2</td>
-                                        <td className="style-row color-token-name">BKD Token</td>
-                                        <td className="style-row">BKDT</td>
-                                        <td className="style-row">0xE41d2489571d322189246DaFA5ebDe1F4699F498</td>
-                                        <td className="style-row">Tokens for tuition fees at Technology DN universtiy</td>
-                                    </tr>
-                                </tbody>
-                                <div>
-                                    
-                                </div>
-                            </Table>
+                        <hr />
+                        <Table id="tbl_tokens_list" striped condensed hover>
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Name</th>
+                                    <th>Symbol</th>
+                                    <th>Address</th>
+                                    <th>Description</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td className="style-row">1</td>
+                                    <td className="style-row color-token-name">DTU Token</td>
+                                    <td className="style-row">DTUK</td>
+                                    <td className="style-row">0xE41d2489571d322189246DaFA5ebDe1F4699F498</td>
+                                    <td className="style-row">Tokens for tuition fees at Duy Tan universtiy</td>
+                                </tr>
+
+                                <tr>
+                                    <td className="style-row">2</td>
+                                    <td className="style-row color-token-name">BKD Token</td>
+                                    <td className="style-row">BKDT</td>
+                                    <td className="style-row">0xE41d2489571d322189246DaFA5ebDe1F4699F498</td>
+                                    <td className="style-row">Tokens for tuition fees at Technology DN universtiy</td>
+                                </tr>
+                            </tbody>
+
+                        </Table>
                         {/* )} */}
                     </div>
                 </div>
+                <div>
+                    <button type="submit" onClick={this.readFromDtbsToTable}>test </button>
+                </div>
             </div>
+
         );
     }
 });
