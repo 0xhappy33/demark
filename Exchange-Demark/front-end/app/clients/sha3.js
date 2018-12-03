1713b0cfb81b50ce0066e76973819954c767b661
quocnhan <quocnhan810@gmail.com>
2018-12-03 15:55:58 +0700

    update web3

--- Exchange-Demark/front-end/app/clients/sha3.js
+++ Exchange-Demark/front-end/app/clients/sha3.js
@@ -1,16 +1,20 @@
+var web3 = require('web3');
+
+console.log(web3.utils.sha3('BuyToken(address,uint256,uint256)'));
+
 
 module.exports = {
     topicBuyToken : ()=>{
-        return web3.sha3('BuyToken(address,uint256,uint256)');
+        return web3.utils.sha3('BuyToken(address,uint256,uint256)');
     },
     topicFundTransfer : ()=>{
-        return web3.sha3('FundTransfer(address,uint256)');
+        return web3.utils.sha3('FundTransfer(address,uint256)');
     },
     topicDeposit : ()=>{
-        return web3.sha3('Deposit(address,uint256)');
+        return web3.utils.sha3('Deposit(address,uint256)');
     },
     topicTransfer : ()=>{
-        return web3.sha3('Transfer(address,address,uint256)');
+        return web3.utils.sha3('Transfer(address,address,uint256)');
     }
 }
 
