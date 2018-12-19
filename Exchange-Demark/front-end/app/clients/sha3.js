37fc5f276271cfa77c4d68e8b5b8cab2a038220e
quocnhan <quocnhan810@gmail.com>
2018-12-19 21:02:42 +0700

    add testTime + edit api

--- Exchange-Demark/front-end/app/clients/sha3.js
+++ Exchange-Demark/front-end/app/clients/sha3.js
@@ -1,8 +1,8 @@
 var web3 = require('web3');
-console.log("Buy ", web3.utils.sha3('BuyToken(address,uint256,uint256)'));
-console.log("Fund ", web3.utils.sha3('FundTransfer(address,uint256)'));
-console.log("Depo ", web3.utils.sha3('Deposit(address,uint256)'));
-console.log("Tra ", web3.utils.sha3('Transfer(address,address,uint256)'));
+// console.log("Buy ", web3.utils.sha3('BuyToken(address,uint256,uint256)'));
+// console.log("Fund ", web3.utils.sha3('FundTransfer(address,uint256)'));
+// console.log("Depo ", web3.utils.sha3('Deposit(address,uint256)'));
+// console.log("Tra ", web3.utils.sha3('Transfer(address,address,uint256)'));
 
 module.exports = {
     topicBuyToken : ()=>{
