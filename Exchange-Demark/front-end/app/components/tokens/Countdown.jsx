1725aa66c38ef43720564a01c4ec7df9a313d403
quocnhan <quocnhan810@gmail.com>
2019-05-13 21:37:57 +0700

    add countdown

---
+++ Exchange-Demark/front-end/app/components/tokens/Countdown.jsx
@@ -0,0 +1,125 @@
+import React, { PropTypes, Component } from 'react';
+
+class Countdown extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      days: 0,
+      hours: 0,
+      min: 0,
+      sec: 0,
+    }
+  }
+
+  componentDidMount() {
+    // update every second
+    this.interval = setInterval(() => {
+      const date = this.calculateCountdown(this.props.date);
+      date ? this.setState(date) : this.stop();
+    }, 1000);
+  }
+
+  componentWillUnmount() {
+    this.stop();
+  }
+
+  calculateCountdown(endDate) {
+    let diff = (Date.parse(new Date(endDate)) - Date.parse(new Date())) / 1000;
+
+    // clear countdown when date is reached
+    if (diff <= 0) return false;
+
+    const timeLeft = {
+      years: 0,
+      days: 0,
+      hours: 0,
+      min: 0,
+      sec: 0,
+      millisec: 0,
+    };
+
+    // calculate time difference between now and expected date
+    if (diff >= (365.25 * 86400)) { // 365.25 * 24 * 60 * 60
+      timeLeft.years = Math.floor(diff / (365.25 * 86400));
+      diff -= timeLeft.years * 365.25 * 86400;
+    }
+    if (diff >= 86400) { // 24 * 60 * 60
+      timeLeft.days = Math.floor(diff / 86400);
+      diff -= timeLeft.days * 86400;
+    }
+    if (diff >= 3600) { // 60 * 60
+      timeLeft.hours = Math.floor(diff / 3600);
+      diff -= timeLeft.hours * 3600;
+    }
+    if (diff >= 60) {
+      timeLeft.min = Math.floor(diff / 60);
+      diff -= timeLeft.min * 60;
+    }
+    timeLeft.sec = diff;
+
+    return timeLeft;
+  }
+
+  stop() {
+    clearInterval(this.interval);
+  }
+
+  addLeadingZeros(value) {
+    value = String(value);
+    while (value.length < 2) {
+      value = '0' + value;
+    }
+    return value;
+  }
+
+  render() {
+    const countDown = this.state;
+
+    return (
+      <div className="Countdown">
+        <ul>
+            <li style={{display: 'inline-block',
+                    fontSize: '0.75em',
+                    // list
+                    listStyleType: 'none',
+                    paddingRight: '1em',
+                    textTransform: 'uppercase'}}><span style={{display: 'block',
+                        fontSize: '2rem'}} id="days">{this.addLeadingZeros(countDown.days)}</span>{countDown.days <= 1 ? 'Day' : 'Days'}</li>
+            <li style={{display: 'inline-block',
+                    fontSize: '0.75em',
+                    // list
+                    listStyleType: 'none',
+                    paddingRight: '1em',
+                    textTransform: 'uppercase'}}><span style={{display: 'block',
+                    fontSize: '2rem'}} id="hours">{this.addLeadingZeros(countDown.hours)}</span>H</li>
+            <li style={{display: 'inline-block',
+                    fontSize: '0.75em',
+                    // list
+                    listStyleType: 'none',
+                    paddingRight: '1em',
+                    textTransform: 'uppercase'}}><span style={{display: 'block',
+                    fontSize: '2rem'}} id="minutes">{this.addLeadingZeros(countDown.min)}</span>M</li>
+            <li style={{display: 'inline-block',
+                    fontSize: '0.75em',
+                    // list
+                    listStyleType: 'none',
+                    paddingRight: '1em',
+                    textTransform: 'uppercase'}}><span style={{display: 'block',
+                    fontSize: '2rem'}} id="seconds">{this.addLeadingZeros(countDown.sec)}</span>S</li>
+        </ul>
+
+      </div>
+    );
+  }
+}
+
+Countdown.propTypes = {
+  date: PropTypes.string.isRequired
+};
+
+Countdown.defaultProps = {
+  date: new Date()
+};
+
+export default Countdown;
